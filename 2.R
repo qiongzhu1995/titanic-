2d07e43e9ed2d8acfbe11f65ef43f43ee824cf43
qiongzhu1995 <50483940+qiongzhu1995@users.noreply.github.com>
2019-05-16 16:58:09 +0800

    Update 2.R

--- 2.R
+++ 2.R
@@ -8,6 +8,62 @@ test <- read.csv('e:/kaggle/Titanic/test.csv')
  mice_output<-complete(mice_mod)
  titanic$Age<-mice_output$Age
  titanic$Fare<-mice_output$Fare
+        titanic$title[titanic$title == ' Mlle']        <- 'Miss' 
+
+titanic$title[titanic$title == ' Ms']          <- 'Miss'
+
+titanic$title[titanic$title == ' Mme']         <- 'Mrs' 
+
+titanic$title[titanic$title == ' Lady']          <- 'Miss'
+
+titanic$title[titanic$title == ' Dona']          <- 'Miss'
+titanic$title[titanic$title == 'Capt']        <- 'Officer' 
+
+titanic$title[titanic$title == 'Col']        <- 'Officer' 
+
+titanic$title[titanic$title == 'Major']   <- 'Officer'
+
+titanic$title[titanic$title == 'Dr']   <- 'Officer'
+
+titanic$title[titanic$title == 'Rev']   <- 'Officer'
+
+titanic$title[titanic$title == 'Don']   <- 'Officer'
+
+titanic$title[titanic$title == 'Sir']   <- 'Officer'
+
+titanic$title[titanic$title == 'the Countess']   <- 'Officer'
+
+titanic$title[titanic$title == 'Jonkheer']   <- 'Officer'  
+ticket.unique <- rep(0, nrow(full))
+
+tickets <- unique(titanic$Ticket)
+
+
+
+for (i in 1:length(tickets)) {
+  
+  current.ticket <- tickets[i]
+  
+  party.indexes <- which(titanic$Ticket == current.ticket)
+  
+  
+  for (k in 1:length(party.indexes)) {
+    
+    ticket.unique[party.indexes[k]] <- length(party.indexes)
+    
+  }
+  
+}
+
+
+
+titanic$ticket.unique <- ticket.unique
+
+titanic$ticket.size[titanic$ticket.unique == 1]   <- 'Single'
+
+titanic$ticket.size[titanic$ticket.unique < 5 &titanic$ticket.unique>= 2]   <- 'Small'
+
+titanic$ticket.size[titanic$ticket.unique >= 5]   <- 'Big'
  titanic <- titanic %>%
   mutate(
    Age = ifelse(is.na(Age), mean(full$Age, na.rm=TRUE), Age),
